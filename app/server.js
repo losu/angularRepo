cc23bc65f546b9969ed8deef1f6308cb83a7d995
unknown <ddba@PCLLE104.gft.com>
2015-11-25 08:44:01 +0100

    update server.js adding other port

--- app/server.js
+++ app/server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var app = express();
 var rootPath = path.resolve(__dirname);
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.use(express.static(rootPath));
 
 app.all('/*', function (req, res) {
@@ -14,4 +17,6 @@ app.all('/*', function (req, res) {
         .sendFile(rootPath + '/index.html');
 });
 
-app.listen(8000);
+app.listen(server_port, server_ip_address);
+
+console.log('Server is running...');
